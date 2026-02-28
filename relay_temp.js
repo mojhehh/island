45d1848205476a9f5cfe920f8e8c3bb7909500ae
mojhehh <sebmojheh@gmail.com>
2026-02-27 20:27:24 -0800

    remove temp scripts and HTML files

--- relay_temp.js
+++
@@ -1,7 +0,0 @@
-
-
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getDatabase, ref, set, onChildAdded, onValue, remove } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
-
-
